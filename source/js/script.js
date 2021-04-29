71ba815c92a6d4ec7b075d8fc23c8e624a054b00
Vafin Artem <a.vafin85@gmail.com>
2021-04-29 13:39:12 +0500

    добаил слайдер cathegories

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,24 @@
 // Общий файл скриптов
 import MenuOpen from './menu-open';
 
+// Открытие меню
 MenuOpen();
+
+// Инициализируем swiper
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 'auto',
+  watchOverflow: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      spaceBetween: 20,
+    },
+    768: {
+      freeMode: true,
+    },
+  }
+});
