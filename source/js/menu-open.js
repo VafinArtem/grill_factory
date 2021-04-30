63d4dbec75fdd30d15f0afde05b020b70736f2bc
Vafin Artem <a.vafin85@gmail.com>
2021-04-30 17:56:05 +0500

    внес правки, частично стилизовал открытие меню

--- source/js/menu-open.js
+++ source/js/menu-open.js
@@ -1,17 +1,14 @@
 
 
 const MenuOpen = () => {
-  const menuElement = document.querySelector(`.js-menu`);
   const openMenuElement = document.querySelector(`.js-menu-open`);
 
-  const getStatusMenu = () => menuElement.classList.contains(`is-open`);
+  const getStatusMenu = () => document.body.classList.contains(`menu-open`);
 
   openMenuElement.addEventListener(`click`, () => {
     if (getStatusMenu()) {
-      menuElement.classList.remove(`is-open`);
       document.body.classList.remove(`menu-open`);
     } else {
-      menuElement.classList.add(`is-open`);
       document.body.classList.add(`menu-open`);
     }
   });
