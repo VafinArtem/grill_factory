0e8996e9a55b822378057534cfe517f8e270b5fe
Vafin Artem <a.vafin85@gmail.com>
2021-05-06 13:30:11 +0500

    добавил в слайдер рестораны touchevents

--- source/js/displacementSlider.js
+++ source/js/displacementSlider.js
@@ -1,4 +1,72 @@
 const displacementSlider = function(opts) {
+  const animationSlide = (slideId) => {
+    mat.uniforms.nextImage.value = sliderImages[slideId];
+    mat.uniforms.nextImage.needsUpdate = true;
+
+    TweenLite.to( mat.uniforms.dispFactor, 1, {
+      value: 1,
+      ease: 'Expo.easeInOut',
+      onComplete: function () {
+        mat.uniforms.currentImage.value = sliderImages[slideId];
+        mat.uniforms.currentImage.needsUpdate = true;
+        mat.uniforms.dispFactor.value = 0.0;
+      }
+    });
+
+    let slideTitleEl = document.getElementById('slide-title');
+    let nextSlideTitle = document.querySelectorAll(`[data-slide-title="${slideId}"]`)[0].innerHTML;
+
+    TweenLite.fromTo( slideTitleEl, 0.5,
+      {
+        autoAlpha: 1,
+        filter: 'blur(0px)',
+        y: 0
+      },
+      {
+        autoAlpha: 0,
+        filter: 'blur(10px)',
+        y: 20,
+        ease: 'Expo.easeIn',
+        onComplete: function () {
+            slideTitleEl.innerHTML = nextSlideTitle;
+
+            TweenLite.to( slideTitleEl, 0.5, {
+                autoAlpha: 1,
+                filter: 'blur(0px)',
+                y: 0,
+            });
+        }
+    });
+  };
+
+  const changeSlide = (isAnimatingStatus, state, paginations, direction) => {
+    if(!isAnimatingStatus) {
+      isAnimatingStatus = true;
+
+      const currentSlide = document.getElementById('pagination').querySelectorAll('.active')[0].dataset.slide;
+
+      if (direction === `ltr`) {
+        if (parseInt(currentSlide, 10) === paginations.length - 1) {
+          state.slideId = 0;
+        } else {
+          state.slideId = parseInt( currentSlide, 10 ) + 1;
+        }
+      } else if (direction === `rtl`) {
+        if (parseInt(currentSlide, 10) === 0) {
+          state.slideId = 2;
+        } else {
+          state.slideId = parseInt( currentSlide, 10 ) - 1;
+        }
+      }
+
+      document.getElementById('pagination').querySelectorAll('.active')[0].classList.remove(`active`);
+      paginations[state.slideId].classList.add(`active`);
+
+      animationSlide(state.slideId);
+      isAnimatingStatus = false;
+    }
+  };
+
   let vertex = `
       varying vec2 vUv;
       void main() {
@@ -96,7 +164,26 @@ const displacementSlider = function(opts) {
 
   let addEvents = function(){
     let pagButtons = Array.from(document.getElementById('pagination').querySelectorAll('button'));
+    const slider = document.getElementById('slider');
     let isAnimating = false;
+
+    const touchState = {
+      start: 0,
+      end: 0,
+      slideId: 0
+    };
+
+    slider.addEventListener(`touchstart`, (evt) => {
+      touchState.start = evt.targetTouches[0].screenX;
+    });
+    slider.addEventListener(`touchend`, (evt) => {
+      touchState.end = evt.changedTouches[0].screenX;
+      if (touchState.end > touchState.start) {
+        changeSlide(isAnimating, touchState, pagButtons, `ltr`);
+      } else if (touchState.end < touchState.start) {
+        changeSlide(isAnimating, touchState, pagButtons, `rtl`);
+      }
+    });
 
     pagButtons.forEach( (el) => {
       el.addEventListener('click', function() {
@@ -108,46 +195,8 @@ const displacementSlider = function(opts) {
 
           let slideId = parseInt( this.dataset.slide, 10 );
 
-          mat.uniforms.nextImage.value = sliderImages[slideId];
-          mat.uniforms.nextImage.needsUpdate = true;
-
-          TweenLite.to( mat.uniforms.dispFactor, 1, {
-            value: 1,
-            ease: 'Expo.easeInOut',
-            onComplete: function () {
-              mat.uniforms.currentImage.value = sliderImages[slideId];
-              mat.uniforms.currentImage.needsUpdate = true;
-              mat.uniforms.dispFactor.value = 0.0;
-              isAnimating = false;
-            }
-          });
-
-          let slideTitleEl = document.getElementById('slide-title');
-          // let slideStatusEl = document.getElementById('slide-status');
-          let nextSlideTitle = document.querySelectorAll(`[data-slide-title="${slideId}"]`)[0].innerHTML;
-          // let nextSlideStatus = document.querySelectorAll(`[data-slide-status="${slideId}"]`)[0].innerHTML;
-
-          TweenLite.fromTo( slideTitleEl, 0.5,
-            {
-              autoAlpha: 1,
-              filter: 'blur(0px)',
-              y: 0
-            },
-            {
-              autoAlpha: 0,
-              filter: 'blur(10px)',
-              y: 20,
-              ease: 'Expo.easeIn',
-              onComplete: function () {
-                  slideTitleEl.innerHTML = nextSlideTitle;
-
-                  TweenLite.to( slideTitleEl, 0.5, {
-                      autoAlpha: 1,
-                      filter: 'blur(0px)',
-                      y: 0,
-                  });
-              }
-          });
+          animationSlide(slideId);
+          isAnimating = false;
         }
       });
     });
