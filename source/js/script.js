4f2b43e88879c4372c46c7dd3fe4af093ab74b84
Vafin Artem <a.vafin85@gmail.com>
2021-05-12 10:59:35 +0500

    работа с оптимизацией на about

--- source/js/script.js
+++ source/js/script.js
@@ -68,7 +68,7 @@ if (document.querySelector(`#instafeed`)) {
   var feed = new Instafeed({
     accessToken: 'IGQVJVeDhQaXFLTWZAEWWVFbHFwVzlFR2JSY1dlVTZAnYWtHcjV5NGp2SjZAMNnlUSUlocnVmc3dwTlV1ZA29aaW5TVU5ZAR3dTXy04VW00Y0VvQUxScXBndmdVTkdBVndVSHR6WldsM1Jn',
     limit: 3,
-    template: '<div class="instagramm__item"><a href="{{link}}" class="instagramm__link"><header class="instagramm__header"><div class="instagramm__logo-box"><img src="/img/pages/about/logo_inst.jpg" alt="Логотип Grill Factory" class="instagramm__logo"></div><p class="instagramm__author">{{model.username}}</p></header><img src="{{image}}" alt="{{caption}}" class="instagramm__img"><p class="instagramm__text"><span class="instagramm__author">{{model.username}}</span> {{caption}}</p></a></div>',
+    template: '<div class="instagramm__item"><a href="{{link}}" class="instagramm__link"><header class="instagramm__header"><div class="instagramm__logo-box"><img src="/img/pages/about/logo_inst.jpg" alt="Логотип Grill Factory" class="instagramm__logo" width="31" height="31"></div><p class="instagramm__author">{{model.username}}</p></header><img src="{{image}}" alt="{{caption}}" class="instagramm__img" width="354" height="369"><p class="instagramm__text"><span class="instagramm__author">{{model.username}}</span> {{caption}}</p></a></div>',
   });
   feed.run();
 }
