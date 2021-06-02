bc5003c765331965da1928eafeb8bda141466b07
Vafin Artem <a.vafin85@gmail.com>
2021-06-02 13:18:52 +0500

    поправил страницу контакты

--- source/js/datePicker.js
+++ source/js/datePicker.js
@@ -6,12 +6,25 @@ const DatePicker = (mobileDetect) => {
   } else if (dateInputElement) {
     const picker = datepicker(`.js-datepicker`, {
       formatter: (input, date, instance) => {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const options = { year: "numeric", month: "long", day: "numeric" };
         const value = date.toLocaleDateString(`ru-RU`, options);
         input.value = value;
       },
-      customDays: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-      customMonths: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+      customDays: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+      customMonths: [
+        "Январь",
+        "Февраль",
+        "Март",
+        "Апрель",
+        "Май",
+        "Июнь",
+        "Июль",
+        "Август",
+        "Сентябрь",
+        "Октябрь",
+        "Ноябрь",
+        "Декабрь",
+      ],
       minDate: new Date(),
       dateSelected: new Date(),
       disableMobile: true,
@@ -21,3 +34,31 @@ const DatePicker = (mobileDetect) => {
 };
 
 export default DatePicker;
+
+// (() => {
+//   let urls = [
+//     "http://mylink1",
+//     "http://mylink2",
+//     "http://mylink3",
+//     "http://mylink4",
+//   ];
+
+//   let idx = 0;
+//   let length = urls.length;
+
+//   const doNext = () => {
+//     let entry = urls[idx];
+
+//     console.log(idx + ":" + entry);
+//     idx++;
+//     console.log([idx, length]);
+//     if (idx < length) {
+//       // Don't do anything special
+//     } else {
+//       idx = 0;
+//     }
+//     setTimeout(doNext, 300);
+//   };
+
+//   doNext();
+// })();
