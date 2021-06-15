9158756af8ff049417106ad1b50acf12f6ce9ae5
Vafin Artem <a.vafin85@gmail.com>
2021-06-15 13:11:47 +0500

    добавил вывод категории по умолчанию если её нет в списке

--- source/js/menu-init.js
+++ source/js/menu-init.js
@@ -1,6 +1,17 @@
 const menuInit = () => {
   const cathegoriesList = document.querySelector(`.js-cathegories`);
 
+  const getCardTemplate = (title, link) => {
+    return `<div class="cathegories__item cathegories__item--menu js-item">
+    <a href="${link}" class="cathegories__link cathegories__link--menu js-link">
+      <div class="cathegories__icon-box cathegories__icon-box--menu">
+        <img src="/img/pages/menu/icon-default.svg" alt="Контур огонька" class="cathegories__icon" width="50" height="50">
+      </div>
+      <h3 class="cathegories__title cathegories__title--menu js-c-title">${title}</h3>
+    </a>
+  </div>`;
+  };
+
   if (cathegoriesList) {
     const titles = cathegoriesList.querySelectorAll(`.js-c-title`);
     const items = cathegoriesList.querySelectorAll(`.js-item`);
@@ -31,11 +42,24 @@ const menuInit = () => {
           MenuLinks = { ...MenuLinks, ...obj };
         });
 
+        const getHref = (title) => {
+          const href = MenuLinks[title];
+          delete MenuLinks[title];
+          return href;
+        };
+
         titles.forEach((title) => {
           title.closest(`.js-link`).href = MenuLinks[title.textContent]
-            ? MenuLinks[title.textContent]
+            ? getHref(title.textContent)
             : title.closest(`.js-item`).remove();
         });
+
+        for (let [key, value] of Object.entries(MenuLinks)) {
+          const newElement = document.createElement(`div`);
+          newElement.innerHTML = getCardTemplate(key, value);
+
+          cathegoriesList.appendChild(newElement.firstChild);
+        }
 
         let currentСategory;
         items.forEach((item) => {
